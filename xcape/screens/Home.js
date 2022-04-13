cd2b4a34d7e584cddb08d6dea2bce8c8fe220a58
WonHee <whlee@guruiot.com>
2022-04-13 20:47:39 +0900

    운필귀정 이미지 업로드

--- xcape/screens/Home.js
+++ xcape/screens/Home.js
@@ -3,11 +3,12 @@ import {View, Text, Button, StyleSheet, TouchableOpacity} from 'react-native';
 import NfcNavigator from '../components/NfcNavigator';
 
 const Home = ({navigation}) => {
+
   return (
     <View>
       <NfcNavigator />
       <Text style={{color: 'black'}}>Home입니당</Text>
-      <Button title="Page1 열기" onPress={() => navigation.navigate('Page1')} />
+      <Button title="HintPage 열기" onPress={() => navigation.navigate('Page1')} />
       <TouchableOpacity
         style={styles.button}
         onPress={() => navigation.navigate('Page2')}>
