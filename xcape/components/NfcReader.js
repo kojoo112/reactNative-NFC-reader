4ee88442246be0f8c2f186427e6717e49d433931
WonHee <whlee@guruiot.com>
2022-04-14 16:00:57 +0900

    nfc 버튼 이벤트 없이 태깅

--- xcape/components/NfcReader.js
+++ xcape/components/NfcReader.js
@@ -3,15 +3,40 @@ import {TouchableOpacity, View, Text, StyleSheet} from 'react-native';
 import NfcManager, {Ndef, NfcTech} from 'react-native-nfc-manager';
 
 const NfcReader = ({setPageName}) => {
+<<<<<<< Updated upstream
   const readNdef = async () => {
     try {
       await NfcManager.requestTechnology(NfcTech.Ndef);
       const tag = await NfcManager.getTag();
       setPageName(Ndef.text.decodePayload(tag.ndefMessage[0].payload));
+=======
+  const [nfcFlag, setNfcFlag] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      readNdef(setPageName);
+    }, 1500);
+  }, [nfcFlag]);
+
+  const readNdef = async () => {
+    try {
+      const supported = await NfcManager.isSupported();
+      const nfcScanning = await NfcManager.isEnabled();
+      if (supported && nfcScanning) {
+        NfcManager.cancelTechnologyRequest();
+        await NfcManager.start();
+        await NfcManager.requestTechnology([NfcTech.Ndef]);
+        const tag = await NfcManager.getTag();
+        tag.ndefStatus = await NfcManager.ndefHandler.getNdefStatus();
+        await setPageName(Ndef.text.decodePayload(tag.ndefMessage[0].payload));
+        NfcManager.cancelTechnologyRequest();
+        setNfcFlag(!nfcFlag);
+      }
+>>>>>>> Stashed changes
     } catch (ex) {
       console.warn('Oops!', ex);
-    } finally {
       NfcManager.cancelTechnologyRequest();
+      setNfcFlag(!nfcFlag);
     }
   };
 
